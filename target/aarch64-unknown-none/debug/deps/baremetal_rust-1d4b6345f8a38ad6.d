21ec8bf1122cfabf7986fda669f3d051a63e2893
Unai Iborra <unroba@gmail.com>
2025-02-25 17:02:17 +0100

    linker changes

---
+++ target/aarch64-unknown-none/debug/deps/baremetal_rust-1d4b6345f8a38ad6.d
@@ -0,0 +1,16 @@
+/home/unab/Desktop/PROGRAMACIÓN/BAREMETAL/BAREMETAL_V2/target/aarch64-unknown-none/debug/deps/libbaremetal_rust-1d4b6345f8a38ad6.rmeta: src/rust_crate/lib.rs src/rust_crate/extern_fn/mod.rs src/rust_crate/extern_fn/uart.rs src/rust_crate/kernel_apps/mod.rs src/rust_crate/kernel_apps/kernel_apps_manager.rs src/rust_crate/kernel_apps/kernel_app.rs src/rust_crate/kernel_apps/apps/mod.rs src/rust_crate/kernel_apps/apps/uart_terminal/mod.rs src/rust_crate/peripherals/mod.rs src/rust_crate/peripherals/uart/mod.rs src/rust_crate/peripherals/uart/uart_rx.rs src/rust_crate/peripherals/uart/uart_tx.rs
+
+/home/unab/Desktop/PROGRAMACIÓN/BAREMETAL/BAREMETAL_V2/target/aarch64-unknown-none/debug/deps/baremetal_rust-1d4b6345f8a38ad6.d: src/rust_crate/lib.rs src/rust_crate/extern_fn/mod.rs src/rust_crate/extern_fn/uart.rs src/rust_crate/kernel_apps/mod.rs src/rust_crate/kernel_apps/kernel_apps_manager.rs src/rust_crate/kernel_apps/kernel_app.rs src/rust_crate/kernel_apps/apps/mod.rs src/rust_crate/kernel_apps/apps/uart_terminal/mod.rs src/rust_crate/peripherals/mod.rs src/rust_crate/peripherals/uart/mod.rs src/rust_crate/peripherals/uart/uart_rx.rs src/rust_crate/peripherals/uart/uart_tx.rs
+
+src/rust_crate/lib.rs:
+src/rust_crate/extern_fn/mod.rs:
+src/rust_crate/extern_fn/uart.rs:
+src/rust_crate/kernel_apps/mod.rs:
+src/rust_crate/kernel_apps/kernel_apps_manager.rs:
+src/rust_crate/kernel_apps/kernel_app.rs:
+src/rust_crate/kernel_apps/apps/mod.rs:
+src/rust_crate/kernel_apps/apps/uart_terminal/mod.rs:
+src/rust_crate/peripherals/mod.rs:
+src/rust_crate/peripherals/uart/mod.rs:
+src/rust_crate/peripherals/uart/uart_rx.rs:
+src/rust_crate/peripherals/uart/uart_tx.rs:
---
+++ target/aarch64-unknown-none/release/deps/baremetal_rust-de577003540f5b70.d
@@ -0,0 +1,16 @@
+/home/unab/Desktop/PROGRAMACIÓN/BAREMETAL/BAREMETAL_V2/target/aarch64-unknown-none/release/deps/libbaremetal_rust-de577003540f5b70.a: src/rust_crate/lib.rs src/rust_crate/extern_fn/mod.rs src/rust_crate/extern_fn/uart.rs src/rust_crate/kernel_apps/mod.rs src/rust_crate/kernel_apps/kernel_apps_manager.rs src/rust_crate/kernel_apps/kernel_app.rs src/rust_crate/kernel_apps/apps/mod.rs src/rust_crate/kernel_apps/apps/uart_terminal/mod.rs src/rust_crate/peripherals/mod.rs src/rust_crate/peripherals/uart/mod.rs src/rust_crate/peripherals/uart/uart_rx.rs src/rust_crate/peripherals/uart/uart_tx.rs
+
+/home/unab/Desktop/PROGRAMACIÓN/BAREMETAL/BAREMETAL_V2/target/aarch64-unknown-none/release/deps/baremetal_rust-de577003540f5b70.d: src/rust_crate/lib.rs src/rust_crate/extern_fn/mod.rs src/rust_crate/extern_fn/uart.rs src/rust_crate/kernel_apps/mod.rs src/rust_crate/kernel_apps/kernel_apps_manager.rs src/rust_crate/kernel_apps/kernel_app.rs src/rust_crate/kernel_apps/apps/mod.rs src/rust_crate/kernel_apps/apps/uart_terminal/mod.rs src/rust_crate/peripherals/mod.rs src/rust_crate/peripherals/uart/mod.rs src/rust_crate/peripherals/uart/uart_rx.rs src/rust_crate/peripherals/uart/uart_tx.rs
+
+src/rust_crate/lib.rs:
+src/rust_crate/extern_fn/mod.rs:
+src/rust_crate/extern_fn/uart.rs:
+src/rust_crate/kernel_apps/mod.rs:
+src/rust_crate/kernel_apps/kernel_apps_manager.rs:
+src/rust_crate/kernel_apps/kernel_app.rs:
+src/rust_crate/kernel_apps/apps/mod.rs:
+src/rust_crate/kernel_apps/apps/uart_terminal/mod.rs:
+src/rust_crate/peripherals/mod.rs:
+src/rust_crate/peripherals/uart/mod.rs:
+src/rust_crate/peripherals/uart/uart_rx.rs:
+src/rust_crate/peripherals/uart/uart_tx.rs:
