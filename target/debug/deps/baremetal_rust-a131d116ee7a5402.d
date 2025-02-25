696ebf619930a3456f0e7f324ab1f00b3ab42d71
Unai Iborra <unroba@gmail.com>
2025-02-25 13:35:51 +0100

    .

---
+++ target/debug/deps/baremetal_rust-a131d116ee7a5402.d
@@ -0,0 +1,16 @@
+C:\Users\Iker\Desktop\UNAI\OTHER\BAREMETAL_V2\target\debug\deps\libbaremetal_rust-a131d116ee7a5402.rmeta: src\rust_crate\lib.rs src\rust_crate\extern_fn\mod.rs src\rust_crate\extern_fn\uart.rs src\rust_crate\kernel_apps\mod.rs src\rust_crate\kernel_apps\kernel_apps_manager.rs src\rust_crate\kernel_apps\kernel_app.rs src\rust_crate\kernel_apps\apps\mod.rs src\rust_crate\kernel_apps\apps\uart_terminal\mod.rs src\rust_crate\peripherals\mod.rs src\rust_crate\peripherals\uart\mod.rs src\rust_crate\peripherals\uart\uart_rx.rs src\rust_crate\peripherals\uart\uart_tx.rs
+
+C:\Users\Iker\Desktop\UNAI\OTHER\BAREMETAL_V2\target\debug\deps\baremetal_rust-a131d116ee7a5402.d: src\rust_crate\lib.rs src\rust_crate\extern_fn\mod.rs src\rust_crate\extern_fn\uart.rs src\rust_crate\kernel_apps\mod.rs src\rust_crate\kernel_apps\kernel_apps_manager.rs src\rust_crate\kernel_apps\kernel_app.rs src\rust_crate\kernel_apps\apps\mod.rs src\rust_crate\kernel_apps\apps\uart_terminal\mod.rs src\rust_crate\peripherals\mod.rs src\rust_crate\peripherals\uart\mod.rs src\rust_crate\peripherals\uart\uart_rx.rs src\rust_crate\peripherals\uart\uart_tx.rs
+
+src\rust_crate\lib.rs:
+src\rust_crate\extern_fn\mod.rs:
+src\rust_crate\extern_fn\uart.rs:
+src\rust_crate\kernel_apps\mod.rs:
+src\rust_crate\kernel_apps\kernel_apps_manager.rs:
+src\rust_crate\kernel_apps\kernel_app.rs:
+src\rust_crate\kernel_apps\apps\mod.rs:
+src\rust_crate\kernel_apps\apps\uart_terminal\mod.rs:
+src\rust_crate\peripherals\mod.rs:
+src\rust_crate\peripherals\uart\mod.rs:
+src\rust_crate\peripherals\uart\uart_rx.rs:
+src\rust_crate\peripherals\uart\uart_tx.rs:
